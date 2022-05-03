de8dcf3aa2359b62d8092783ad37482cba925811
cquarles14 <catlynne.quarles@sinclair.edu>
2022-05-03 02:02:17 -0400

    Initial commit.

--- Function_Templates_Part_B/main.cpp
+++ Function_Templates_Part_B/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  Function_Templates_Part_B
-//
-//  Created by Catlynne Quarles on 5/3/22.
-//
+// Catlynne Quarles
+// CIS 1202 R02
+// May 3, 2022
+// Function Templates Part B
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+
